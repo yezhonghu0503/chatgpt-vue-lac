40637dfa7b55c90250b5517c4f6404a540f112d2
yezhonghu0503 <211736726@qq.com>
2023-05-18 01:24:36 +0800

    feat:chat主框架&左侧菜单

--- src/pages/chat.vue
+++ src/pages/chat.vue
@@ -1,17 +1,12 @@
 <template>
-    <div class="w-[60vw] h-[90vh] bg-rg-chat-border rounded-[30px] flex justify-end p-4">
-        <!-- <div class="w-[60vw] h-[100%] bg-rg-chat rounded-[50px]"> -->
-        <div class="w-[45vw] h-[100%] bg-rg-chat rounded-[30px] flex">
-            <div class="w-[30vw] h-[100%] border-r-2 border-indigo-500">
-                <div class="w-[100%] h-[65px] border-b-2 border-indigo-500 flex justify-start items-center p-5">
-                    <span class='text-2xl font-bold'>ChatGPT Session window</span>
-                </div>
-            </div>
-            <div class="w-[15vw] h-[65px] border-b-2 border-indigo-500"></div>
-        </div>
+    <div class="w-[60vw] h-[90vh] bg-rg-chat-border rounded-[30px] flex justify-end p-4 pl-0">
+        <chatMenu />
+        <chatMain />
+        
     </div>
 </template>
 <script lang="ts" setup>
-
+import chatMain from '../components/chat-main/chatMain.vue';
+import chatMenu from '../components/chat-menu/chatMenu.vue'
 </script>
 <style lang="less" scoped></style>
